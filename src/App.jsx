12bf1d3a82780c9efcafd9ea52cf58a75cb451f4
Gabriel Oliver <gabrielo@withnubo.com>
2025-05-18 13:09:49 -0400

    feat: make text-center for small screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <div class="flex flex-col items-center justify-center gap-4 w-full h-screen bg-neutral-950">
+      <div class="text-center flex flex-col items-center justify-center gap-4 w-full h-screen bg-neutral-950">
         <h1 class="font-mono text-5xl font-bold text-neutral-50">
           Gabriel Oliver
         </h1>

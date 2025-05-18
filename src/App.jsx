eeef48624d5ac1b8a7b9c3ac508cbc55b16c7f09
Gabriel Oliver <gabrielo@withnubo.com>
2025-05-18 12:45:53 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <div class="flex flex-col items-center justify-center gap-4 w-full h-screen bg-neutral-950">
+        <h1 class="font-mono text-5xl font-bold text-neutral-50">
+          Gabriel Oliver
+        </h1>
+        <p class="font-medium text-neutral-400">Building cloud experiences</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
